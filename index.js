c74b3ea328798de0eef08666fc8f676f933c7362
olinga-abdulaziz <abdulazizolinga17@gmail.com>
2026-08-03 02:43:50 +0300

    aaa

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ const allowedOrigins = ['http://localhost:3000', 'http://127.0.0.1:3000'];
 app.use(
   cors({
     origin(origin, callback) {
+      console.log("Incoming Origin:", origin);
       if (!origin) return callback(null, true);
       if (allowedOrigins.indexOf(origin) !== -1) {
         return callback(null, true);
